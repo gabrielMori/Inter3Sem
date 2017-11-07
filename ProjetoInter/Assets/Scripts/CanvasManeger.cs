cd010046ae0af93b5ef1932c20d0cf6d7b36f12a
Unknown <junqueirademello@gmail.com>
2017-11-07 19:43:48 -0200

    canvas criado com script e menus com layout certinho, botões ainda n programados

---
+++ ProjetoInter/Assets/Scripts/CanvasManeger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManeger : MonoBehaviour {
+    public GameObject pause;
+    public GameObject mapa;
+    bool ativado=false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (ativado == false)
+            {
+                Time.timeScale = 0;
+                pause.SetActive(true);
+                ativado = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pause.SetActive(false);
+                ativado = false;
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Time.timeScale = 0;
+            mapa.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            Time.timeScale = 1;
+            mapa.SetActive(false);
+        }
+    }
+}
