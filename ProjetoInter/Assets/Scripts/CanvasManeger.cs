29622389ccfe0e0dea4100975e2b7545fcea6804
Unknown <junqueirademello@gmail.com>
2017-11-11 14:28:14 -0200

    ainda n funcionando menu inicial

--- ProjetoInter/Assets/Scripts/CanvasManeger.cs
+++ ProjetoInter/Assets/Scripts/CanvasManeger.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class CanvasManeger : MonoBehaviour {
     public GameObject pause;
     public GameObject mapa;
+    public GameObject inicio;
     bool ativado=false;
 	// Use this for initialization
 	void Start () {
-		
+        inicio.SetActive(true);
+        Time.timeScale = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.P))
+
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (ativado == false)
             {
