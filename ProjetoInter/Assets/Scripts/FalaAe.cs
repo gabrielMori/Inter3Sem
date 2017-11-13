9d3488520af00c22df4cc60b97e177d91547a9ae
Unknown <junqueirademello@gmail.com>
2017-11-13 18:08:06 -0200

    resolvendo as falas

---
+++ ProjetoInter/Assets/Scripts/FalaAe.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalaAe : MonoBehaviour {
+    public GameObject objDestruido;
+    public GameObject dialogo;
+    public Collider2D acionador;
+    bool umavezporra = true;
+    bool umavezdenovoporra = true;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (acionador.enabled == false&&umavezporra==true)
+        {
+            dialogo.SetActive(true);
+            umavezporra = false;
+        }
+        if (umavezporra == false)
+        {
+            
+            Invoke("podeIr", 1);
+        }
+        if (objDestruido.activeSelf){
+            return;
+        } else
+            Invoke("podeIr", 1);
+    }
+    void podeIr()
+    {
+        if (Input.anyKey)
+        {
+            dialogo.SetActive(false);
+        }
+            
+    }
+}
