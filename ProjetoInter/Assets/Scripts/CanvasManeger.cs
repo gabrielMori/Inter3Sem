d6f4b3b1f204d54872cc9fddb9e7b09ccb92ee95
DESKTOP-GM3UNI9\user <gabriel.mori.pinto.garcia@gmail.com>
2017-11-23 20:49:44 -0200

    bgdzhggzdfhgd

--- ProjetoInter/Assets/Scripts/CanvasManeger.cs
+++ ProjetoInter/Assets/Scripts/CanvasManeger.cs
@@ -5,107 +5,112 @@ using UnityEngine.UI;
 
 public class CanvasManeger : MonoBehaviour
 {
-    public GameObject[] multSons;
-    public GameObject inventario;
-    public GameObject pause;
-    public GameObject mapa;
-    bool peguei = false;
-    bool ativado = false;
-    int veAi;
-    private void Start()
-    {
-        veAi = PlayerPrefs.GetInt("som");
-        if (veAi == 1)
-            Tira_som();
-        else
-            Toca_som();
-    }
+	public GameObject[] multSons;
+	public GameObject inventario;
+	public GameObject pause;
+	public GameObject mapa;
+	bool peguei = false;
+	bool ativado = false;
+	int veAi;
+	private void Start()
+	{
+		veAi = PlayerPrefs.GetInt("som");
+		if (veAi == 1)
+			Tira_som();
+		else
+			Toca_som();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-                if (ativado == false)
-                {
-                    Time.timeScale = 0;
-                    pause.SetActive(true);
-                    ativado = true;
-                }
-                else
-                {
-                    Time.timeScale = 1;
-                    pause.SetActive(false);
-                    ativado = false;
-                }
-            
-        }
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-                if (ativado == false)
-                {
-                    Time.timeScale = 0;
-                    inventario.SetActive(true);
-                    ativado = true;
-                }
-                else
-                {
-                    Time.timeScale = 1;
-                    inventario.SetActive(false);
-                    ativado = false;
-                }
-            
-        }
-        if (peguei == false)
-        {
-            if (Input.GetKeyUp(KeyCode.M))
-            {
-                Time.timeScale = 1;
-                mapa.SetActive(false);
-                peguei = true;
-            }
-        }
-            if (Input.GetKeyDown(KeyCode.M) && peguei == true)
-            {
-                Time.timeScale = 0;
-                mapa.SetActive(true);
-            }
-            if (Input.GetKeyUp(KeyCode.M) && peguei == true)
-            {
-                Time.timeScale = 1;
-                mapa.SetActive(false);
-            }
-    }
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if(!inventario.activeSelf && !mapa.activeSelf){
+				if (ativado == false)
+				{
+					Time.timeScale = 0;
+					pause.SetActive(true);
+					ativado = true;
+				}
+				else
+				{
+					Time.timeScale = 1;
+					pause.SetActive(false);
+					ativado = false;
+				}
+			}
 
-    public void Continue() {
-        Time.timeScale = 1;
-        pause.SetActive(false);
-        ativado = false;
+		}
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			if(!pause.activeSelf && !mapa.activeSelf){
+				if (ativado == false)
+				{
+					Time.timeScale = 0;
+					inventario.SetActive(true);
+					ativado = true;
+				}
+				else
+				{
+					Time.timeScale = 1;
+					inventario.SetActive(false);
+					ativado = false;
+				}
+			}
+		}
+		if (peguei == false)
+		{
+			if (Input.GetKeyUp(KeyCode.M))
+			{
+				Time.timeScale = 1;
+				mapa.SetActive(false);
+				peguei = true;
+			}
+		}
+		if(!pause.activeSelf && !inventario.activeSelf){
+			if (Input.GetKeyDown(KeyCode.M) && peguei == true)
+			{
+				Time.timeScale = 0;
+				mapa.SetActive(true);
+			}
+			if (Input.GetKeyUp(KeyCode.M) && peguei == true)
+			{
+				Time.timeScale = 1;
+				mapa.SetActive(false);
+			}
+		}
+	}
 
-    }
+	public void Continue() {
+		Time.timeScale = 1;
+		pause.SetActive(false);
+		ativado = false;
 
-    public void Continue_inventario()
-    {
-        Time.timeScale = 1;
-        inventario.SetActive(false);
-        ativado = false;
+	}
 
-    }
-    public void Tira_som()
-    {
-        PlayerPrefs.SetInt("som",1);
-        for (int i = 0; i < multSons.Length; i++)
-        {
-            multSons[i].SetActive(false);
-        }
-    }
-    public void Toca_som()
-    {
-        PlayerPrefs.SetInt("som", 0);
-        for (int i = 0; i < multSons.Length; i++)
-        {
-            multSons[i].SetActive(true);
-        }
-    }
+	public void Continue_inventario()
+	{
+		Time.timeScale = 1;
+		inventario.SetActive(false);
+		ativado = false;
+
+	}
+	public void Tira_som()
+	{
+		PlayerPrefs.SetInt("som",1);
+		for (int i = 0; i < multSons.Length; i++)
+		{
+			multSons[i].SetActive(false);
+		}
+	}
+	public void Toca_som()
+	{
+		PlayerPrefs.SetInt("som", 0);
+		for (int i = 0; i < multSons.Length; i++)
+		{
+			multSons[i].SetActive(true);
+		}
+	}
 }
